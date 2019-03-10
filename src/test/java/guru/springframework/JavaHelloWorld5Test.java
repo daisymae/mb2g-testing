cac092f72f5558baee11871ac384eedbbff73669
daisymae <daisymae.orcadiafarm@gmail.com>
2019-03-10 11:55:02 -0500

    Add JUnit 5 to Spock
    My attempt at the assignment to add JUnit 5 to Spock. It's not right,
    though, as my JUnit5 test is not being run. The other 2 are still being
    run. Why is that? On to get answers to resolve this.
    
    This is a known issue with groovy and Java 11.

---
+++ src/test/java/guru/springframework/JavaHelloWorld5Test.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JUnit5Test {
+
+  @Test
+  void aTestForJUnit5() {
+    System.out.println("JUnit 5 test begin");
+    System.out.println("JUnit 5 test end");
+  }
+}
